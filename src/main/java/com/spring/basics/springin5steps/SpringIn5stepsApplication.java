23b01339bf218b3589cf6b5137ca88328ff68b47
prashantRmishra <pm7044872@gmail.com>
2021-10-12 19:32:07 +0530

    new commits

---
+++ src/main/java/com/spring/basics/springin5steps/SpringIn5stepsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.basics.springin5steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5stepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5stepsApplication.class, args);
+	}
+
+}
